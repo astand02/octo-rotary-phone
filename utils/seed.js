0c31524be07bc3edd97032849062f17748e733be
astand02 <135463433+astand02@users.noreply.github.com>
2023-11-20 16:01:33 -0600

    added seeds

--- utils/seed.js
+++ utils/seed.js
@@ -1,43 +1,28 @@
 const connection = require('../config/connection');
-const { User, Thought } = require('../models');
-const { getRandomName, getRandomThoughts } = require('./data');
+const { User, Thought, Reaction } = require('../models');
+const mongoose = require("mongoose");
 
-connection.on('error', (err) => err);
+const users = [
+  {
+    username: "John",
+    email: "1234@email.com",
+    thought: []
+},
+];
 
-connection.once('open', async () => {
-  console.log('connected');
-  // Delete the collections if they exist
-  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
-  if (thoughtCheck.length) {
-    await connection.dropCollection('thoughts');
-  }
+console.log(connection);
 
-  let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
-  if (userCheck.length) {
-    await connection.dropCollection('users');
-  }
+connection.once("open", async () => {
+  console.log("connected");
 
-  const users = [];
-  const thoughts = getRandomThoughts(10);
+  // Drop existing students
+  await User.deleteMany({});
 
-  for (let i = 0; i < 20; i++) {
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-
-    users.push({
-      first,
-      last,
-      age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
-    });
-  }
-
+  // Adds seed data to database
   await User.collection.insertMany(users);
-  await Thought.collection.insertMany(thoughts);
 
-  // loop through the saved videos, for each video we need to generate a video response and insert the video responses
   console.table(users);
-  console.table(thoughts);
-  console.info('Seeding complete! 🌱');
+  console.info("Seeding complete! 🌱");
   process.exit(0);
 });
+
